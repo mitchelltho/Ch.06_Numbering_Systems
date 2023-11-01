1f349d0dfb308780b6a75fcce982851293e8048e
mitchelltho <mitchelltho@urbandaleschools.com>
2023-11-01 08:58:51 -0500

    Sussy commit message

--- 6.0_Jedi_Training.py
+++ 6.0_Jedi_Training.py
@@ -3,19 +3,19 @@
 
 
 1.) Convert Binary 101010 to Hexadecimal. (1pt)
-42 in hexa
+                  2A
 2.) Convert Binary 11101001 to Octal. (1pt)
                   351
 3.) Convert Hexadecimal FC to Binary. (1pt)
-
+                 11111100
 4.) Convert Hexadecimal 1F to Decimal. (1pt)
-
+                   31
 5.) Convert Hexadecimal #FAAFBD to RGB. (1pt)
-
+                 (250, 175, 189)
 6.) Convert Octal 70 to Binary. (1pt)
-
+                  1101010
 7.) Convert RGB (32,128,64) to Hexadecimal. (1pt)
-
+                  208040
 8.) Convert 01000001, 01010011, 01000011, 01001001, 01001001 to ASCII. (1pt)
 
 9.) Reached 2000 on the Binary Game? Upload screen shot!  (7pt)
